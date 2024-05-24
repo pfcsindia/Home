5fe52d28b047d7a3e93afc486446a5c9db26f802
pfcsindia <harshaldahivalikar.pro@gmail.com>
2024-05-25 03:05:30 +0530

    ktgtg

---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1716586501/wp-content/uploads/uag-plugin/assets/0/uag-js-36.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-80b9eb16', {"layout":"number","heading":"Clients Served &amp; More Counting","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":15000,"totalNumber":15000,"decimalPlaces":0,"animationDuration":800,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function(){
+	UAGBTableOfContents._run( {"mappingHeaders":[true,true,true,true,true,true],"scrollToTop":false,"makeCollapsible":false}, '.uagb-block-a9464636' );
+} );
+ });
